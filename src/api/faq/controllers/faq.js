def05376367cc40768217e250ad70076f342b044
Andres Celis <andresc@150porciento.com>
2022-02-07 21:33:56 -0500

    chore: update schemas and controllers

--- src/api/faq/controllers/faq.js
+++ src/api/faq/controllers/faq.js
@@ -1,9 +1,30 @@
-'use strict';
+"use strict";
 
 /**
  *  faq controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::faq.faq');
+module.exports = createCoreController("api::faq.faq", () => {
+    return {
+        async find(ctx) {
+            const faqs = await strapi.query("api::faq.faq").findOne({
+                populate: {
+                    questions: true,
+                },
+
+                where: {
+                    locale: ctx.request?.query?.locale ?? "en",
+                },
+            });
+
+            if (Array.isArray(faqs?.questions)) {
+                return faqs?.questions;
+            }
+
+            ctx.status = 404;
+            return;
+        },
+    };
+});
