d085bd52fd24d49d305aae24dedbde4bfbdb3888
Andres Celis <andresc@150porciento.com>
2022-03-06 17:35:24 -0500

    chore: dump dependencies

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  auth: {
-    secret: env('ADMIN_JWT_SECRET', 'f96652e0c4c857b8d24bc0aaa7d070d5'),
-  },
+    auth: {
+        secret: env("ADMIN_JWT_SECRET", "f96652e0c4c857b8d24bc0aaa7d070d5"),
+    },
 });
