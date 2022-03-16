beabfba009df7001480d69d4f20c216134b01cc3
Andres Celis <andresc@150porciento.com>
2022-03-15 19:16:14 -0500

    chore: update body limits

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,11 +1,21 @@
 module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::favicon',
-  'strapi::public',
+    "strapi::errors",
+    "strapi::security",
+    "strapi::cors",
+    "strapi::logger",
+    "strapi::query",
+    "strapi::favicon",
+    "strapi::public",
+
+    {
+        name: "strapi::body",
+        config: {
+            formLimit: "10MB",
+            jsonLimit: "10MB",
+            textLimit: "10MB",
+            formidable: {
+                maxFileSize: 8 * 1024 * 1024,
+            },
+        },
+    },
 ];
