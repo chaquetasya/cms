12e56a824641e8e5580e7f65a4aaaecfabf29d3a
Andres Celis <andresc@150porciento.com>
2022-03-17 18:27:39 -0500

    chore(WIP): create `discount` and `cart` type

--- src/api/return-policy/routes/return-policy.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * return-policy router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::return-policy.return-policy');
--- src/api/return-policy/services/return-policy.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * return-policy service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::return-policy.return-policy');
